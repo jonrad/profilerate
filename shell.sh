e5704eade8ce5d4e096486ff13b1eb40d53c6ad2
Jon Radchenko <jonrad@gmail.com>
2023-02-17 19:07:19 +0100

    get rid of env cruf and fix zsh

--- shell.sh
+++ shell.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
-DIR=$(dirname "$0")
-export PROFILERATE_ID=$(basename $DIR)
-if [ -x "$(command -v zsh)" ]; then
-  export PROFILERATE_SHELL=zsh
-  $DIR/zshi.sh $DIR/profilerate.sh
-elif [ -x "$(command -v bash)" ]; then
-  export PROFILERATE_SHELL=bash
-  bash --init-file $DIR/profilerate.sh
-else
-  export PROFILERATE_SHELL=sh
-  sh
-fi
+profilerate_shell() {
+  local DIR=$(dirname "$0")
+  local PATH=$PATH
+
+  # TODO fix dupe paths
+  if [ -x "$SHELL" ]; then
+    PATH="$(dirname "$SHELL"):$PATH"
+  fi
+
+  export PROFILERATE_ID=$(basename $DIR)
+  if [ -x "$(command -v zsh)" ]; then
+    export PROFILERATE_SHELL=zsh
+    $DIR/zshi.sh $DIR/profilerate.sh -l
+  elif [ -x "$(command -v bash)" ]; then
+    export PROFILERATE_SHELL=bash
+    bash --init-file $DIR/profilerate.sh
+  else
+    export PROFILERATE_SHELL=sh
+    export ENV=$DIR/profilerate.sh
+    sh
+  fi
+}
+
+profilerate_shell
