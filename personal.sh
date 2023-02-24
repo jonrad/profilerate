09b2240c7c82fd1be392cab9f0d7d7b0a528a59e
Jon Radchenko <jonrad@gmail.com>
2023-02-24 13:50:06 +0100

    better handling of args

--- personal.sh
+++ personal.sh
@@ -5,9 +5,39 @@
 # alias dr="profilerate_docker_run"
 # alias de="profilerate_docker_exec"
 
+alias k="profilerate_kubectl"
+alias de="profilerate_docker_exec"
+alias dr="profilerate_docker_run"
+alias ke="profilerate_kubernetes"
+alias s="profilerate_ssh"
+alias kns="profilerate_kns"
+alias kctx="profilerate_kctx"
+
+if [ "$PROFILERATE_SHELL" = "zsh" ]; then
+  alias ls="ls -lastrG"
+else
+  alias ls="ls -lastr --color=auto"
+fi
+
+export PAGER="less -XF"
+alias less="less -XF"
+
+# better search history
+if [ -n "$(command -v bind)" ]; then
+  bind '"\e[A": history-search-backward'
+  bind '"\e[B": history-search-forward'
+  bind '"\e[1;3C": forward-word'
+  bind '"\e[1;3D": backward-word'
+fi
+
+if [ ! $PROFILERATE_SHELL = "zsh" ]; then
+  export PS1="\[\033]0;\u|\h|\$PWD||\$PREPROMPT|\007\[\e[32m\]\$PWD\[\e[m\] \\$ "
+fi
+
 ### Shell specific configurations ###
 if [ "$PROFILERATE_SHELL" = "zsh" ]; then
   # Put your zsh specific configuration here
+  . $PROFILERATE_DIR/personal.zsh
   echo "Profilerate: Logged into zsh"
 elif [ "$PROFILERATE_SHELL" = "bash" ]; then
   # Put your bash specific configuration here
--- profilerate.sh
+++ profilerate.sh
@@ -90,22 +90,20 @@ if [ -x "$(command -v kubectl)" ]; then
 
   # replicates our configuration to a remote env before running an interactive shell
   profilerate_kubectl () {
-    emulate -L sh > /dev/null 2>&1
-    for POD; do true; done
-    TOTAL_ARGS=$#
-    ARGS=""
-    if [ $# -gt 1 ]; then
-      for i in $(seq 1 "$($TOTAL_ARGS - 1)")
-      do
-        ARG=$(eval "echo \" \$$i\"")
-        ARGS="$ARGS $ARG"
-      done
-    fi
+    eval "POD=\"\${$#}\""
+    __pop_n=$(($# - 1))
+    __pop_index=0
+    __pop_arguments=""
+    while [ $__pop_index -lt $__pop_n ]; do
+      __pop_index=$((__pop_index+1))
+      __pop_arguments="$__pop_arguments \"\${$__pop_index}\""
+    done
+    eval "set -- $__pop_arguments"
 
     # TODO fix for args having spaces
-    DEST=$(kubectl exec $@ -- sh -c "$_PROFILERATE_CREATE_DIR") && \
+    DEST=$(kubectl exec "$@" $POD -- sh -c "$_PROFILERATE_CREATE_DIR") && \
       kubectl cp "$PROFILERATE_DIR/." $ARGS "$POD:$DEST" && \
-      kubectl exec -it "$@" -- "$DEST/shell.sh"
+      kubectl exec -it "$@" $POD -- "$DEST/shell.sh"
   }
 fi
 
@@ -114,24 +112,22 @@ if [ -x "$(command -v ssh)" ]; then
   profilerate_debug "Detected ssh"
   # ssh [args] HOST to ssh to host and replicate our environment
   profilerate_ssh () {
-    emulate -L sh > /dev/null 2>&1
-    for HOST; do true; done
-    TOTAL_ARGS=$#
-    ARGS=""
-    if [ $# -gt 1 ]; then
-      for i in $(seq 1 "$($TOTAL_ARGS - 1)")
-      do
-        ARG=$(eval "echo \" \$$i\"")
-        ARGS="$ARGS $ARG"
-      done
-    fi
+    eval "HOST=\"\${$#}\""
+    __pop_n=$(($# - 1))
+    __pop_index=0
+    __pop_arguments=""
+    while [ $__pop_index -lt $__pop_n ]; do
+      __pop_index=$((__pop_index+1))
+      __pop_arguments="$__pop_arguments \"\${$__pop_index}\""
+    done
+    eval "set -- $__pop_arguments"
 
     # TODO fix for args with spaces
     # Also... scp is annoying. because scp $DIR/. doesn't work in all systems, we need to delete
     # the directory and then hope it can get created again with scp
-    DEST=$(ssh $ARGS "$HOST" "DIR=\$($_PROFILERATE_CREATE_DIR); test -d \$DIR && rmdir \$DIR && echo \$DIR" 2>/dev/null) && \
-      scp -r $ARGS "$PROFILERATE_DIR" "$HOST:$DEST" >/dev/null 2>&1 && \
-      ssh -t $ARGS "$HOST" "sh -lc \"chmod 700 '$DEST';'$DEST/shell.sh'\""
+    DEST=$(ssh "$@" "$HOST" "DIR=\$($_PROFILERATE_CREATE_DIR); test -d \$DIR && rmdir \$DIR && echo \$DIR" 2>/dev/null) && \
+      scp -r "$@" "$PROFILERATE_DIR" "$HOST:$DEST" >/dev/null 2>&1 && \
+      ssh -t "$@" "$HOST" "sh -lc \"chmod 700 '$DEST';'$DEST/shell.sh'\""
   }
 fi
 
