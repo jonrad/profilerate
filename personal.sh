36664b6ca20793e86089d7783354d9aaa44d2254
Jon Radchenko <jonrad@gmail.com>
2023-02-25 18:15:29 +0100

    fixed personal.sh file

--- personal.sh
+++ personal.sh
@@ -1,49 +1,26 @@
 ### This is your file. Make modifications here
 
+# Warning: make sure to use the most portable code in this file. If
+# you want to use something that is shell specific, source it in the if statement below
+
 ### Global - Applies to all shells ###
+# Examples:
 # alias ls="ls -l"
 # alias dr="profilerate_docker_run"
 # alias de="profilerate_docker_exec"
 
-alias k="profilerate_kubectl"
-alias de="profilerate_docker_exec"
-alias dr="profilerate_docker_run"
-alias ke="profilerate_kubernetes"
-alias s="profilerate_ssh"
-alias kns="profilerate_kns"
-alias kctx="profilerate_kctx"
-
-if [ "$PROFILERATE_SHELL" = "zsh" ]; then
-  alias ls="ls -lastrG"
-else
-  alias ls="ls -lastr --color=auto"
-fi
-
-export PAGER="less -XF"
-alias less="less -XF"
-
-# better search history
-if [ -n "$(command -v bind)" ]; then
-  bind '"\e[A": history-search-backward'
-  bind '"\e[B": history-search-forward'
-  bind '"\e[1;3C": forward-word'
-  bind '"\e[1;3D": backward-word'
-fi
-
-if [ ! $PROFILERATE_SHELL = "zsh" ]; then
-  export PS1="\[\033]0;\u|\h|\$PWD||\$PREPROMPT|\007\[\e[32m\]\$PWD\[\e[m\] \\$ "
-fi
-
 ### Shell specific configurations ###
 if [ "$PROFILERATE_SHELL" = "zsh" ]; then
   # Put your zsh specific configuration here
-  . $PROFILERATE_DIR/personal.zsh
   echo "Profilerate: Logged into zsh"
+  # You can also create other filea to source and they'll run in the appropriate context
+  # (eg you can use zsh specific formatting/functions/etc)
+  # . $PROFILERATE_DIR/personal.zsh.sh
 elif [ "$PROFILERATE_SHELL" = "bash" ]; then
   # Put your bash specific configuration here
   echo "Profilerate: Logged into bash"
 else
   # This is usually something like almquist shell
   # Put your most standard configuration here
-  echo "Profilerate: Logged into an unknown shell"
+  echo "Profilerate: Logged into sh"
 fi
