07acb25bac48f00fecf3b4dfd310947d216fffc5
Jon Radchenko <jonrad@users.noreply.github.com>
2023-02-28 22:41:55 +0100

    better interactive

--- run-tests-interactive.sh
+++ run-tests-interactive.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env sh
-docker run -it -e --privileged --rm \
+CONTAINER_ID=$(docker run --detach --privileged --rm \
+  --init \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$PWD:/code/" \
   --entrypoint sh \
   --name "profilerate-test-runner" \
   jonrad/profilerate-test-runner:latest \
-  -c "export INTERACTIVE=1; bash; unset INTERACTIVE; . ./testing/tests/setup_suite.bash; teardown_suite"
+  -c "sleep infinity")
 
-# TODO get this to work
-#profilerate_docker_run -e PROFILERATE_DEBUG=1 --privileged --rm \
-#  -v /var/run/docker.sock:/var/run/docker.sock \
-#  -v "$PWD:/code/" \
-#  jonrad/profilerate-test-runner:latest
+if [ -z "$(command -v "profilerate_docker_exec")" ]
+then
+  echo "Couldn't find profilerate_docker_exec. If you're using profilerate, you can source this file and we'll profilerate into the test runner. eg:"
+  echo "source $0"
+  docker exec -it -e 'INTERACTIVE=1' $CONTAINER_ID bash
+else
+  profilerate_docker_exec -e 'INTERACTIVE=1' $CONTAINER_ID
+fi
+
+echo Cleaning Up
+docker exec -it $CONTAINER_ID sh -c ". ./testing/tests/setup_suite.bash; teardown_suite"
+docker stop $CONTAINER_ID >/dev/null
+echo Bye and thanks for all the fish
