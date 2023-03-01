860ef5be02688ef2fb2dc5acc3152419efe6bed6
Jon Radchenko <jon.radchenko@dominodatalab.com>
2023-03-01 22:32:59 +0100

    Handle no args and also fix an untar bug

--- profilerate.sh
+++ profilerate.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-#V3
 
 if [ -z "${PROFILERATE_DIR:-}" ]; then
   PROFILERATE_DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
@@ -50,22 +49,24 @@ _profilerate_copy () {
     rsync -e "$RSH" --rsync-path="$DEST/" -r "$PROFILERATE_DIR/." rsync:. >/dev/null 2>&1 && return
   fi
 
-  # Try our implementation of tar
+  # Try to use tar
+  # TODO: how portable is --exclude? We need it to avoid changing perms on the directory we created
   if [ -x "$(command -v tar)" ]; then
-    tar -c -f - -C "$PROFILERATE_DIR" . | eval "$RSH sh -c 'cd $DEST && tar -o -x -f -'" >/dev/null 2>&1 && return
+    tar -c -f - -C "$PROFILERATE_DIR/" . | eval "$RSH sh -c 'cd $DEST && tar --exclude ./ -o -x -f -'" >/dev/null >&2 && return
   fi
 
+  # If all else fails, transfer the files one at a time
   # Loop through all the files and transfer them via cat
-  # note we're optimizing for connection count, so we do multiple loops
+  # note we're optimizing for connection count
   # i wonder if we can do this as one command... (echo "Abc" > foo; echo "foo" > bar;)
-  # certainly with something like base64 or od, but is that more efficient?
+  # certainly with something like base64 or od, but is that more efficient? And is it more portable?
   cd $PROFILERATE_DIR
   FILES=$(find .)
   LAST_IFS=$IFS
 
   MKDIR=""
-  echo "$FILES" | while IFS= read -r FILENAME 
-  do 
+  echo "$FILES" | while IFS= read -r FILENAME
+  do
     if [ -d "$FILENAME" ]
     then
       if [ "$FILENAME" != "." ]
@@ -77,8 +78,8 @@ _profilerate_copy () {
   eval "$RSH sh -c 'cd $DEST;$MKDIR'"
 
   CHMOD=""
-  echo "$FILES" | while IFS= read -r FILENAME 
-  do 
+  echo "$FILES" | while IFS= read -r FILENAME
+  do
     if [ -f "$FILENAME" ]
     then
       CHMOD="${CHMOD}chmod $(stat -f %Mp%Lp $FILENAME) $FILENAME;"
@@ -94,8 +95,6 @@ _profilerate_copy () {
 if [ -x "$(command -v docker)" ]; then
   # replicates our configuration to a container before running an interactive bash
   profilerate_docker_cp () {
-    #for CONTAINER; do true; done
-
     RSH="docker exec -i $@"
     DEST=$(docker exec "$@" sh -c "$_PROFILERATE_CREATE_DIR") && \
       _profilerate_copy "$RSH" "$DEST" >&2 && \
@@ -106,7 +105,12 @@ if [ -x "$(command -v docker)" ]; then
   }
 
   profilerate_docker_exec () {
-    for CONTAINER; do true; done
+    if [ "$#" = 0 ]
+    then
+      echo 'profilerate_docker_exec has the same args as "docker exec", except for COMMAND. See below' >&2
+      docker exec --help >&2
+      return
+    fi
 
     DEST=$(profilerate_docker_cp "$@")
     if [ -n "$DEST" ]
@@ -119,8 +123,15 @@ if [ -x "$(command -v docker)" ]; then
   }
 
   profilerate_docker_run () {
-    # TODO: This can be optimized by using docker run shell + docker attach using streaming input
-    CONTAINER=$(docker run -it --detach --init --entrypoint sh "$@" -c 'while true; do sleep 60; done')
+    if [ "$#" = 0 ]
+    then
+      echo 'profilerate_docker_run has the same args as "docker run", except for COMMAND. See below' >&2
+      docker run --help >&2
+      return
+    fi
+
+    # TODO: This may be optimized by using docker run shell + docker attach using streaming input
+    CONTAINER=$(docker run -it --detach --init --entrypoint sh "$@" -c 'while true; do sleep 600; done')
 
     if [ -n "$CONTAINER" ]; then
       profilerate_docker_exec "$CONTAINER"
@@ -133,7 +144,12 @@ fi
 if [ -x "$(command -v kubectl)" ]; then
   # replicates our configuration to a remote env before running an interactive shell
   profilerate_kubectl () {
-    # TODO fix for args having spaces
+    if [ "$#" = 0 ]
+    then
+      echo 'profilerate_kubectl has the same args as "kubectl exec", except for COMMAND. See below' >&2
+      kubectl exec --help >&2
+      return
+    fi
     DEST=$(kubectl exec -i "$@" -- sh -c "$_PROFILERATE_CREATE_DIR")
 
     if [ -n "$DEST" ]
@@ -152,6 +168,12 @@ fi
 if [ -x "$(command -v ssh)" ]; then
   # ssh [args] HOST to ssh to host and replicate our environment
   profilerate_ssh () {
+    if [ "$#" = 0 ]
+    then
+      echo 'profilerate_ssh has the same args as ssh, except for [command]. See below' >&2
+      ssh >&2
+      return
+    fi
     RSH="ssh $@"
 
     # TODO fix for args with spaces
