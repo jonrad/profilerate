39fd5a70619e2d0d1f89883e9139376b7d2ad18c
Jon Radchenko <jon.radchenko@dominodatalab.com>
2023-03-03 15:16:38 +0100

    better handling of switching shells in current terminal

--- shell.sh
+++ shell.sh
@@ -11,13 +11,13 @@ profilerate_shell() {
   fi
 
   if [ -x "$(command -v zsh)" ]; then
-    export PROFILERATE_SHELL="zsh"
+    PROFILERATE_SHELL="zsh"
     "$PROFILERATE_DIR/shells/zsh.sh" "$PROFILERATE_DIR/profilerate.sh" -l
   elif [ -x "$(command -v bash)" ]; then
-    export PROFILERATE_SHELL="bash"
+    PROFILERATE_SHELL="bash"
     bash --init-file "$PROFILERATE_DIR/shells/bash.sh"
   else
-    export PROFILERATE_SHELL="sh"
+    PROFILERATE_SHELL="sh"
     export ENV="$PROFILERATE_DIR/shells/sh.sh"
     sh
   fi
