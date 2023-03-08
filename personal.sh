76a4acda4766a16aedeb88d2e7164c5d052b23e9
Jon Radchenko <jonrad@users.noreply.github.com>
2023-03-07 20:19:59 -0500

    cleanup personal

--- personal.sh
+++ personal.sh
@@ -12,15 +12,15 @@
 ### Shell specific configurations ###
 if [ "$PROFILERATE_SHELL" = "zsh" ]; then
   # Put your zsh specific configuration here
-  echo "Profilerate: Logged into zsh"
+  :
   # You can also create other filea to source and they'll run in the appropriate context
   # (eg you can use zsh specific formatting/functions/etc)
   # . $PROFILERATE_DIR/personal.zsh.sh
 elif [ "$PROFILERATE_SHELL" = "bash" ]; then
   # Put your bash specific configuration here
-  echo "Profilerate: Logged into bash"
+  :
 else
   # This is usually something like dash/almquist shell
   # Put your most portable configuration here
-  echo "Profilerate: Logged into sh"
+  :
 fi
